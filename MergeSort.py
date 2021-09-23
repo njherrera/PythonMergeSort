43f617cef24a183c8e48fa95b1207010f829ea09
njherrera <nathan.j.herrera@gmail.com>
2021-09-22 18:16:01 -0700

    first commit, just outlining pseudocode

---
+++ MergeSort.py
@@ -0,0 +1,27 @@
+def mergeSort(list):
+    # if list is length 1, return list
+
+    # make one empty list for left side, one for right side
+
+    # add first half of og list to left, right half to right side
+
+    # call mergeSort on left
+    # call mergeSort on right
+
+    # return
+
+def merge(left, right):
+    # make new empty list (result)
+
+    # while left and right are not empty:
+        # if the first item of left is <= first item of right, append first item of left to result
+        # else append first item of right to result
+
+    # now either left or right is not empty
+    # while left is not empty:
+        # append first item of left to result
+
+    # while right is not empty:
+        # append first item of right to result
+
+    # return result
